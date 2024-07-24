09566203d3ea5bb7a3a57cc48f708e875fa07e27
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-07-24 10:25:15 +0100

    update github

--- js/flag.js
+++ js/flag.js
@@ -1,19 +1,29 @@
 // Fetch data from the REST Countries API
-fetch("https://restcountries.com/v3.1/all")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // Process the data
-    data.forEach((country) => {
-      console.log(
-        `Country: ${country.name.common}, Population: ${country.population}, Region: ${country.region}`
-      );
+
+function fetch3Countries() {
+  let url = "https://restcountries.com/v3.1/all";
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      // Process data
+      data.forEach((country) => {
+        console.log(
+          `Country: ${country.name.common}, Population: ${country.population}, Region: ${country.region}, ${country.flags.svg}, ${country.independent}`
+        );
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
     });
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+}
+
+fetch3Countries();
+
+fetch3Countries();
